67dc2f1f1d1468499ac7e4df14413d83fabea6f6
unknown <jocelynhuang193@outlook.com>
2021-06-23 21:02:44 +1000

    fix deploy.sh \ => /

--- bene_store_backend/bin/deploy.sh
+++ bene_store_backend/bin/deploy.sh
@@ -1,4 +1,4 @@
-eb init --profile housoul
+eb init
 
 eb setenv POSTGRES_HOST=database-2.c3jzo4tbgeo2.us-east-1.rds.amazonaws.com POSTGRES_DB=collection POSTGRES_USER=xinxin POSTGRES_PASSWORD=internet
 
